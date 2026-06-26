5a9a77001c2911d44bc9a11e111b15afe3342a2b
Billy Conard <billy.conard@pcc.edu>
2026-06-26 09:47:05 -0700

    Wrapped the image constants in square brackets to accept the array of images.

--- src/Components/PreOrder.js
+++ src/Components/PreOrder.js
@@ -4,7 +4,7 @@ import ImageCycler from './ImageCycler';
 const PreOrder = () => {
 
     // Store hero images for home page cycler
-    const preOrders = useState([
+    const [preOrders] = useState([
         { src: '../../Images/preOrder1.jpg', alt: 'A black and white picture of T-Shirts hanging in front of a painted wall.', id: 1 },
         { src: '../../Images/preOrder2.jpg', alt: 'A young woman holding her hat and posing in front of a wooden building.', id: 2 },
         { src: '../../Images/preOrder3.jpg', alt: 'A young man wearing all black with a ski mask standing on railroad tracks.', id: 3 },
